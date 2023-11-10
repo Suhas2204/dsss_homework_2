dba754f1d9cb77a6fc57435bb050b470004097b9
SuhasJagtap <119647374+Suhas2204@users.noreply.github.com>
2023-11-10 18:06:46 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,40 +1,70 @@
 import random
 
 
-def function_A(min, max):
+def yield_random_integers(min_value, max_value):
     """
-    Random integer.
+    This will generate the random integer within specified range
+    
+    min_value sets the minimimum value for random integer
+    max_value sets the maximum value for random integer
+
+    Which returns :int(integer)
+    
     """
-    return random.randint(min, max)
+    return random.randint(min_value, max_value)
 
 
-def function_B():
+def yield_random_operators():
+    '''
+    This generates the random arithmetic operator ('+', '-', '*')
+
+    Which returns: str(string)
+
+
+    '''
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def perform_operation(num_1, num_2, operator):
+    '''
+    This will perform the arithmetic operation between numbers and operators
+
+    which will return a Tuple containing a arithmetic problem as a string and the correct answer
+
+    '''
+    p = f"{num_1} {operator} {num_2}"
+    if operator == '+': 
+        answer = num_1 + num_2
+    elif operator == '-': 
+        answer = num_1 - num_2
+    else: answer = num_1 * num_2
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    '''
+    which will conduct a math quiz game with the user
+
+    '''
+    score = 0
+    total_questions = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for i in range(total_questions):
+        num_1 =  yield_random_integers(1, 10); 
+        num_2 =  yield_random_integers(1, 5); 
+        operator = yield_random_operators()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = perform_operation(num_1, num_2, operator)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        try:
+            user.answer = int(input("Your answer: "))
+        except Valuerror:
+            print ('invalid input,Please put a valid integer)
+            continue
 
-        if useranswer == ANSWER:
+        if user__answer == correct_answer:
             print("Correct! You earned a point.")
             s += -(-1)
         else:
